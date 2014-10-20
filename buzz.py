e5b8d4bcab35a4621d327e7ab34de52eec27287f
Raenn <danbelsey@hotmail.co.uk>
2014-10-20 18:20:15 +0100

    Incorporated buzzword getter into graph drawer. Painfully basic for now.

--- buzz.py
+++ buzz.py
@@ -1,12 +1,15 @@
 from ggplot import *
 import pandas as pd
 import numpy as np
+import word_loader
+
+words = word_loader.get_buzzwords().split()[0:3]
 
 #TODO: improve how data is passed in. Lots of different ways; see http://pandas.pydata.org/pandas-docs/stable/dsintro.html#dsintro
-df = pd.DataFrame([(0, 30), (1, 25), (2, 22), (3, 1)], columns=["Days since manager in office", "# of 'synergy' uses"])
+df = pd.DataFrame([(words[0], 1), (words[1], 2), (words[2], 3)], columns=["Buzzword", "# of uses"])
 
-foobar = ggplot(df, aes(x="Days since manager in office", y="# of 'synergy' uses")) +\
-	geom_line() +\
+foobar = ggplot(df, aes(x='Buzzword', fill='Buzzword', weight='# of uses')) +\
+	geom_bar() +\
 	geom_hline(yintercept=2, color='red')
 
 print foobar
--- word_loader.py
+++ word_loader.py
@@ -1,5 +1,5 @@
 import urllib2
 
-def getBuzzwords():
+def get_buzzwords():
 	response = urllib2.urlopen("http://www.buzzwordipsum.com/buzzwords?type=words&paragraphs=1")
 	return response.readlines()[0]
